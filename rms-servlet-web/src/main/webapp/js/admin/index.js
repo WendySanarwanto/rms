1a94ec8fd9d6d0e6500e20b300347b816f361a04
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-05-23 17:17:49 +0800

    Added TODO comments which explain the logic of how to implement the Edit, Delete logic

--- rms-servlet-web/src/main/webapp/js/admin/index.js
+++ rms-servlet-web/src/main/webapp/js/admin/index.js
@@ -21,8 +21,6 @@ $(document).ready(() => {
     // Reload the table list with the retrieved records    
     loadMainContainer(tabId, dataSet);
 
-    // TODO: Hide spinning progress when the AJAX Call is finished.
-    // TODO: Show Error Toast when the AJAX Call return error.
     // TODO: Show the hidden table list
   });
 
@@ -33,15 +31,15 @@ $(document).ready(() => {
 function flattenRetrievedData(retrievedData) {
   let dataSet = [];
   let propNames = [];
-  if (Array.isArray(retrievedData)){
+  if (Array.isArray(retrievedData)) {
     if (retrievedData.length > 0) {
       propNames = Object.keys(retrievedData[0]);
       // propNames.splice(0, 1); // Remove id column
     }
 
-    dataSet = retrievedData.map(data => {      
+    dataSet = retrievedData.map(data => {
       const dataSetItem = [];
-      for(const propName of propNames) {
+      for (const propName of propNames) {
         dataSetItem.push(data[propName]);
       }
       return dataSetItem;
@@ -54,48 +52,103 @@ function flattenRetrievedData(retrievedData) {
 function loadMainContainer(tabId, dataSet) {
   const adminMainContainerUrl = getAdminMainContainerUrl(tabId);
   if (adminMainContainerUrl) {
-    
+
     // Render retrieved records on table list
     // TODO: Add Action column on the table list which has 2 buttons inside : delete, edit.    
     $(`#${ADMIN_MAIN_CONTAINER_ID}`).load(adminMainContainerUrl, () => {
       $(`#${ADMIN_CONTENT_ID}`).load(`views/shared/tableList.html`, () => {
-        var tableList = 
+        var tableList =
           $(`.${TABLE_LIST_CLASS}`).DataTable({
             data: dataSet,
             columns: getTableListHeaderNames(tabId),
-            processing: true ,
+            processing: true,
             columnDefs: [{
               targets: -1,
               data: null,
               defaultContent: `&nbsp;&nbsp;&nbsp;&nbsp;<button class="ui button basic teal">Edit</button>&nbsp;&nbsp;&nbsp;&nbsp;<button class="ui button basic red">Delete</button>`
             }]
           });
-        
-        $(`.${TABLE_LIST_CLASS} tbody`).on('click', 'button', () => {
 
+        $(`.${TABLE_LIST_CLASS} tbody`).on('click', 'button', (e) => {
+          var clickedData = tableList.row($(e.target).parents('tr')).data();
+          var clickedButtonName = e.target ? e.target.innerText : null;
+          console.log(`[DEBUG] - <admin.index> tabId: ${tabId}`);
+          switch (clickedButtonName) {
+            case 'Edit':
+              // TODO: Show edit modal dialog
+              // TODO: Handle on confirm:
+              //        - change the edit button's style to progressing
+              //        - get proposed value from the modal dialog instance
+              //        - call Change Record API
+              //        - restore the edit button's progressing style when process is finished
+              //        - show error toast when edit process is failing
+              break;
+            case `Delete`:
+              // TODO: Show delete pop up confirmation
+              // TODO: handle on confirm: 
+              //        - change the delete button's style to progressing
+              //        - call Delete Record api
+              //        - restore the delete button's progressing style when process is finished.
+              //        - show error toast when delete process is failing
+              break;
+          }
+          // console.log(`[DEBUG] - <admin.index> clickedData: \n`, clickedData);
         });
+
+        // Re-styling the Table List's Search input
+        const filterElement = $(`.dataTables_filter label`);
+        filterElement.addClass("ui input");
+        filterElement.contents().filter(function() { return this.nodeType === 3; }).remove();
+        $(filterElement).children('input').prop("placeholder", "Search . . . ");
       });
     });
   }
 }
 
-function getTableListHeaderNames(tabId){
+function getTableListHeaderNames(tabId) {
   // TODO: Generate the header names from retrieved data
   switch (tabId) {
     case ADMIN_USER_ACCOUNT_TAB_ID:
-      return [
-        { title: "ID" },
-        { title: "Full Name" }, 
-        { title: "Email address" }, 
-        { title: "Role" }, 
-        { title: "Actions"}
+      return [{
+          title: "ID"
+        },
+        {
+          title: "Full Name"
+        },
+        {
+          title: "Email address"
+        },
+        {
+          title: "Role"
+        },
+        {
+          title: "Actions"
+        }
       ];
     case ADMIN_SE_TAB_ID:
-      return [ { title: "Full Name" }, { title: "Email address"}, { title: "Actions"} ];
+      return [{
+        title: "Full Name"
+      }, {
+        title: "Email address"
+      }, {
+        title: "Actions"
+      }];
     case ADMIN_PM_TAB_ID:
-      return [ { title: "Full Name" }, { title: "Email address"}, { title: "Actions"} ];
+      return [{
+        title: "Full Name"
+      }, {
+        title: "Email address"
+      }, {
+        title: "Actions"
+      }];
     case ADMIN_PROJECTS_TAB_ID:
-      return [ { title: "Name" }, { title: "Description" }, { title: "Actions"} ];
+      return [{
+        title: "Name"
+      }, {
+        title: "Description"
+      }, {
+        title: "Actions"
+      }];
     default:
       return null;
   }
